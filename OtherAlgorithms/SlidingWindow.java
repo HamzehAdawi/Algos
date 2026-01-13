3e388f7c567a218b21ab216efc5eee466c5f5e96
HamzehAdawi <ronham12@yahoo.com>
2026-01-12 20:59:57 -0600

    SlidingWindow Algorithm

---
+++ OtherAlgorithms/SlidingWindow.java
@@ -0,0 +1,33 @@
+public class SlidingWindow {
+
+	// Sliding window algorithm with the objective to
+	// print the contigous sequences with the sum of 8.
+	public void slide(int[] arr, int targetSum) {
+
+		int left = 0;
+		int currSum = 0;
+
+		for (int right =0; right < arr.length; right++) {
+			currSum += arr[right];
+
+			while (currSum > targetSum) {
+				currSum -= arr[left];
+				left++;
+			}
+			if (currSum == targetSum) {
+				System.out.println("Indexes from " + left + " to "+ right + 
+				" total " + targetSum);
+			} 
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		int[] arr = {2, 32, 2, 51, 3, 1, 4};
+		int targetSum = 8;
+
+		SlidingWindow slidingWindow = new SlidingWindow();
+		slidingWindow.slide(arr, targetSum);			
+	}
+}
